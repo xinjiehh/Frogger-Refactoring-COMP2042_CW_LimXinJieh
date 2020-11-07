f9946040444fbf88c951577f0fe9ac768a8c0011
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-11-08 06:10:02 +0800

    Initial commit

---
+++ Frogger/src/main/java/Frogger/Digit.java
@@ -0,0 +1,21 @@
+package Frogger;
+
+import javafx.scene.image.Image;
+
+public class Digit extends Actor{
+	int dim;
+	Image im1;
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Digit(int n, int dim, int x, int y) {
+		im1 = new Image("file:src/main/java/Frogger/"+n+".png", dim, dim, true, true);
+		setImage(im1);
+		setX(x);
+		setY(y);
+	}
+	
+}
