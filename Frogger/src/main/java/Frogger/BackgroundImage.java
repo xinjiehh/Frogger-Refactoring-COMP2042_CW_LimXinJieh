c5b8255a5bcf9756f7c450719f41660ff799357a
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-11-08 20:50:16 +0800

    Fix: add ColorAdjust effect to fix game background contrast

--- Frogger/src/main/java/Frogger/BackgroundImage.java
+++ Frogger/src/main/java/Frogger/BackgroundImage.java
@@ -1,8 +1,12 @@
 package Frogger;
 
+import java.io.File;
+
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 
-public class BackgroundImage extends Actor{
+//actor is imageView
+public class BackgroundImage extends Actor {
 
 	@Override
 	public void act(long now) {
@@ -12,7 +16,27 @@ public class BackgroundImage extends Actor{
 	
 	public BackgroundImage() {
 		setImage(new Image("file:src/main/resources/gameBackground.png", 600, 800, false, true));
+		adjustColor(); 
 		
 	}
+	
+	
+	public BackgroundImage(String link) {
+		setImage(new Image(link, 800, 800, false, true));
+		
+	}
+	
+	
+	/**
+	 * reference see <a href="https://www.tutorialspoint.com/javafx/color_adjust_effect.htm">tutorialspoint</a>
+	 * 
+	 */
+	private void adjustColor() {
+		ColorAdjust colorAdjust = new ColorAdjust();
+		colorAdjust.setContrast(2);     
+	    setEffect(colorAdjust);  //Applying color adjust effect to the ImageView node 
+	}
+
+
 
 }
