a0b5f6f2179f9329df1319eff921ce0ff1435855
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-11-09 18:50:36 +0800

    Refactor: rename 'MyStage' file as 'GamePane'

--- Frogger/src/main/java/Frogger/GamePane.java
+++ Frogger/src/main/java/Frogger/GamePane.java
@@ -6,14 +6,14 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
-public class MyStage extends World{
+public class GamePane extends World{
 	MediaPlayer mediaPlayer;
 	@Override
 	public void act(long now) {
 		
 	}
 	
-	public MyStage() {
+	public GamePane() {
 		
 //		mediaPlayer.play();
 //		mediaPlayer.setOnEndOfMedia(new Runnable() {
@@ -31,6 +31,8 @@ public class MyStage extends World{
 	public void playMusic() {
 		String musicFile = "src/main/resources/Frogger Main Song Theme (loop).mp3";   
 		Media sound = new Media(new File(musicFile).toURI().toString());
+		System.out.println("music" + new File(musicFile).toURI());
+		
 		mediaPlayer = new MediaPlayer(sound);
 		mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 	    mediaPlayer.play();
