7da52aa9f5ed3d955a226e109e74a3330e4faad5
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-11-10 16:21:07 +0800

    Refactor: create constant variable 'size', standardize method for setting image

--- Frogger/src/main/java/Frogger/End.java
+++ Frogger/src/main/java/Frogger/End.java
@@ -3,6 +3,7 @@ package Frogger;
 import javafx.scene.image.Image;
 
 public class End extends Actor{
+	private static final int SIZE = 60;
 	boolean activated = false;
 	@Override
 	public void act(long now) {
@@ -12,12 +13,16 @@ public class End extends Actor{
 	public End(int x, int y) {
 		setX(x);
 		setY(y);
-		setImage(new Image("file:src/main/resources/End.png", 60, 60, true, true));
+		setEndImage("file:src/main/resources/End.png");
 	}
 	
 	public void setEnd() {
-		setImage(new Image("file:src/main/resources/FrogEnd.png", 70, 70, true, true));
+		setEndImage("file:src/main/resources/FrogEnd.png");
 		activated = true;
+	}
+	
+	private void setEndImage(String url) {
+		setImage(new Image(url, SIZE, SIZE, true, true));
 	}
 	
 	public boolean isActivated() {
