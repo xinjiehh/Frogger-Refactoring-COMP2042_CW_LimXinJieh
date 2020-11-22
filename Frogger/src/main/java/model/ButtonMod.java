17c53448644b99cf5d56ab519855011d7a106158
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-11-23 02:22:45 +0800

    Refactor: move ButtonMod class into Model package

--- Frogger/src/main/java/model/ButtonMod.java
+++ Frogger/src/main/java/model/ButtonMod.java
@@ -1,8 +1,9 @@
-package Frogger;
+package model;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import Frogger.Main;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
@@ -14,48 +15,56 @@ import javafx.scene.text.Font;
  * constructor initializes fixed button width, height, font, with customizable text
  * removed xPos yPos from constructor
  */
-
+//<%=outterBoxWidth;%>
 public class ButtonMod extends Button {
+	private static final String BUTTON_STYLE = "-fx-background-color: transparent; -fx-background-image: url('file:" + Main.RESOURCE_PATH;
+	private static final String FONT_PATH =  Main.FONT_PATH + "joystix monospace.ttf"; //TODO new FileInputStream, no need 'file:'
+	private static final String BUTTON_PRESSED = BUTTON_STYLE + "button_pressed.png" + "');";
+	private static final String BUTTON_UP = BUTTON_STYLE + "button.png" + "');";
 
-	private final String FONT_PATH = "src/main/resources/kenvector_future.ttf"; //TODO new FileInputStream, no need 'file:'
-	private final String BUTTON_PRESSED_STYLE = "-fx-background-color: transparent; -fx-background-image: url('file:src/main/resources/button_pressed.png');";
-	private final String BUTTON_STYLE = "-fx-background-color: transparent; -fx-background-image: url('file:src/main/resources/button.png');";
-
-	
-	
 	public ButtonMod (String text) {
 		setText(text);
 		setButtonFont();
 		setPrefWidth(190);
 		setPrefHeight(49);
-		setStyle(BUTTON_STYLE);
+		setStyle(BUTTON_UP);
 		initialiseButtonListeners(); 
 		
 	}
-	
+		
+
+	/**
+	 * This method sets the default font for button text
+	 */
 	
 	private void setButtonFont() {
 		try {
 			
-			setFont(Font.loadFont(new FileInputStream(FONT_PATH), 23));
+			setFont(Font.loadFont(new FileInputStream(FONT_PATH), 20));
 
 		} catch (FileNotFoundException e){
 			
-			setFont(Font.font("Sans Serif", 23));
+			setFont(Font.font("Sans Serif", 20));
 			
 			
 		}
 	}
-	
+	/**
+	 * This method sets the style for a pressed button
+	 */
 	private void setButtonPressedStyle() {
-		setStyle(BUTTON_PRESSED_STYLE);
+		setStyle(BUTTON_PRESSED);
 		setPrefHeight(45);
 		setLayoutY(getLayoutY()+4);
 		
 	}
 	
+	/**
+	 * This method sets the style for a released button
+	 */
+	
 	private void setButtonReleasedStyle() {
-		setStyle(BUTTON_STYLE);
+		setStyle(BUTTON_UP);
 		setPrefHeight(49);
 		setLayoutY(getLayoutY()-4);
 		
@@ -63,11 +72,13 @@ public class ButtonMod extends Button {
 	
 	
 	/**
-	 * onMouseReleased, Pressed, Entered, Exited
+	 * This method registers event handlers for mouse events by defining 
+	 * methods to handle mouse presence (entrance / exit) and mouse press 
+	 * (pressed / released) on this object.
 	 */
 	
 	private void initialiseButtonListeners() {
-		
+
 		setOnMousePressed(new EventHandler<MouseEvent>() {
 			
 			@Override
