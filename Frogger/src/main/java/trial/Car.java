c37afc74bbaf8da6a90e924de2c5cbe8b49f067d
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-01 20:44:07 +0800

    Refactor: remove redundant code

---
+++ Frogger/src/main/java/trial/Car.java
@@ -0,0 +1,14 @@
+package trial;
+
+import frogger.constant.OBSTACLE_TYPE;
+import frogger.model.NPC.Obstacle;
+
+public class Car extends Obstacle {
+
+	public Car() {
+		super(OBSTACLE_TYPE.CAR);
+	}
+
+
+
+}
