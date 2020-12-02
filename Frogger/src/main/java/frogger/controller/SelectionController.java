006ded3d554314de225a6f56deb3166a8c58731d
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-02 18:31:32 +0800

    Feat: create selection screen for users to choose key controls

--- Frogger/src/main/java/frogger/controller/SelectionController.java
+++ Frogger/src/main/java/frogger/controller/SelectionController.java
@@ -3,8 +3,8 @@ package frogger.controller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import frogger.constant.AudioPlayer;
 import frogger.constant.FilePath;
+import frogger.util.AudioPlayer;
 //import frogger.constant.GameLevel;
 //import frogger.constant.GameMode;
 //import frogger.util.MusicPlayer;
@@ -22,33 +22,25 @@ public class SelectionController {
   @FXML private Button mode;
   @FXML private Button controls;
   @FXML private Button start;
-  @FXML private TextField nicknameA;
-  @FXML private TextField nicknameB;
   @FXML private Button musicon;
   @FXML private Button musicoff;
 
-  private int maxTextLength = 8;
-	public enum Controls {
-		A, //WASD
-		B, //ARROW
-		MULTI;
-	}
-	
-	public enum Mode {
-		SINGLE,
-		DOUBLE;
-	}
-
+  public enum Controls {
+	  A, //WASD
+	  B, //ARROW
+  }
+  
+  public enum Mode {
+	  SINGLE,
+	  DOUBLE;
+  }
 
   @FXML
   public void initialize() {
 	initFont();
-	updateMusicButton();
-    initTextListener(nicknameA);
-    initTextListener(nicknameB);
+	switchMusicButton();
     mode.setUserData(Mode.SINGLE);
     controls.setUserData(Controls.A);
-    hideTextFieldB();
   }
   
   private void initFont() {
@@ -59,6 +51,7 @@ public class SelectionController {
 			font = Font.loadFont(new FileInputStream(FilePath.DEFAULT_FONT), 20);
 
 		} catch (FileNotFoundException e){
+			
 			System.out.println("error load font for selection");
 			font = Font.font("Sans Serif", 20);
 
@@ -69,44 +62,12 @@ public class SelectionController {
 	  start.setFont(font);
   }
 
-  private void initTextListener(TextField textField) {
-    textField.textProperty().addListener((obs, oldValue, newValue) -> {
-      String temp = newValue;
-      temp = temp.replaceAll("[^a-zA-Z]", "");
-      if (temp.length() > maxTextLength) {
-        temp = temp.substring(0, maxTextLength);
-      }
-      textField.setText(temp);
-    });
-  }
+
 
   @FXML
   public void switchMode() {
-    if (mode.getUserData() == Mode.SINGLE) {
-      mode.setUserData(Mode.DOUBLE);
-      mode.setText("< DOUBLE MODE >");
-      showTextFieldB();
-      
-    } else {
-      mode.setUserData(Mode.SINGLE);
-      mode.setText("< SINGLE MODE >");
-      hideTextFieldB();
-    }
   }
 
-  private void showTextFieldB() {
-    nicknameB.setVisible(true);
-    nicknameB.setDisable(false);
-    controls.setVisible(false);
-    controls.setDisable(true);
-  }
-
-  private void hideTextFieldB() {
-    nicknameB.setVisible(false);
-    nicknameB.setDisable(true);
-    controls.setVisible(true);
-    controls.setDisable(false);
-  }
 
   @FXML
   public void switchControls() {
@@ -118,10 +79,9 @@ public class SelectionController {
 		        break;
 		      case B:
 		    	controls.setUserData(Controls.A);
-		    	controls.setText("<       WASD       >");
+		    	controls.setText("<    WASD    >");
 		        break;
-		      case MULTI:
-		    	 break;
+
 		    } 
 	  
 
@@ -129,24 +89,22 @@ public class SelectionController {
 
   @FXML
   public void startGame() {
-	  if(mode.getUserData() == Mode.DOUBLE)
-		  controls.setUserData(Controls.MULTI);
-    ScreenController.INSTANCE.startGame((Controls) controls.getUserData(), nicknameA.getText(), nicknameB.getText());
+    ScreenController.INSTANCE.startGame((Controls) controls.getUserData());
   }
 
   @FXML
   public void switchMusicState() {
-    if (AudioPlayer.INSTANCE.isOn()) {
-      AudioPlayer.INSTANCE.setVolume(0);
+    if (AudioPlayer.INSTANCE.isOn())
+    	AudioPlayer.INSTANCE.setVolume(0);
 
-    } else {
-      AudioPlayer.INSTANCE.setVolume(0.5);
+     else
+    	 AudioPlayer.INSTANCE.setVolume(0.5);
 
-    }
-    updateMusicButton();
+    switchMusicButton();
   }
 
-  private void updateMusicButton() {
+  
+  private void switchMusicButton() {
 	  
 	  if (AudioPlayer.INSTANCE.isOn()) {
 		  	musicon.setVisible(true);
