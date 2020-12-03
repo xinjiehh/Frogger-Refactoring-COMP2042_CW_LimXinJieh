b25613cce5a4ffada50f7dc7f2adc0ef2da743ea
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-03 21:10:48 +0800

    Docs: add Javadocs to MenuController and ScoreScreenController

--- Frogger/src/main/java/frogger/controller/MenuController.java
+++ Frogger/src/main/java/frogger/controller/MenuController.java
@@ -10,45 +10,66 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * This enum class implements singleton method. It acts as controller
+ * for MenuScreen (View) which handles the different actions of the 
+ * menu buttons such as showing selection screen, showing info screen
+ * and exiting the application
+ * 
+ */
 public enum MenuController {
 	INSTANCE;
 	
 	private Pane pane = new Pane();
-	FXMLLoader loader;
+	private FXMLLoader loader;
 	
+	/**
+	 * This method loads and shows the selection page from FXML for 
+	 * users to choose their controls
+	 */
 	public void viewSelection() {
 		try {
-		      this.loader = new FXMLLoader(getClass().getResource(FilePath.VIEW_SELECT));
-		      this.pane = loader.load();
-		      Main.getPrimaryStage().getScene().setRoot(pane);
-		      Main.getPrimaryStage().show();
+			this.loader = new FXMLLoader(getClass().getResource(FilePath.VIEW_SELECT));
+		    this.pane = loader.load();
+		    Main.getPrimaryStage().getScene().setRoot(pane);
+		    Main.getPrimaryStage().show();
 		      
 		} catch (IOException e) {
-		      e.printStackTrace();
+			System.out.println("Error loading selection page");
+			e.printStackTrace();
 		}
 
 	}
 	
-	
+	/**
+	 * This method loads and shows the info page from FXML which has 
+	 * a brief overview of the game
+	 */
 	public void showInfo() {
 		try {
-		      FXMLLoader loader = new FXMLLoader((getClass().getResource("/view/info.fxml")));
-		      Pane root = loader.load();
-		      Stage infoStage = new Stage();
-		      infoStage.setScene(new Scene(root));
-		      infoStage.initOwner(Main.getPrimaryStage().getScene().getWindow());
-		      infoStage.setResizable(false);
-		      infoStage.setTitle("Info");
-		      infoStage.show();
-		    } catch (IOException e) {
-		    	System.out.println("Error loading info window in Screen Controller");
-		    	e.printStackTrace();
-		    }
+			FXMLLoader loader = new FXMLLoader((getClass().getResource("/view/info.fxml")));
+		    this.pane = loader.load();
+		   
+		    Stage infoStage = new Stage();
+		    infoStage.setTitle("Info");
+		    infoStage.setResizable(false);
+		    infoStage.initOwner(Main.getPrimaryStage().getScene().getWindow());
+		    infoStage.setScene(new Scene(pane));
+		   
+		    
+		    infoStage.show();
+		    
+		  } catch (IOException e) {
+			  System.out.println("Error loading info window in Screen Controller");
+			  e.printStackTrace();
+		  }
 	}
 	
+	/**
+	 * This method handles the exiting of the application
+	 */
 	public void exit() {
 		Platform.exit();
-		//Main.getPrimaryStage().close();
 		//https://stackoverflow.com/questions/26619566/javafx-stage-close-handler
 	}
 
--- Frogger/src/main/java/frogger/controller/ScoreScreenController.java
+++ Frogger/src/main/java/frogger/controller/ScoreScreenController.java
@@ -5,45 +5,130 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
 import java.util.ArrayList;
 import java.util.List;
-import frogger.model.GameModel;
 
-public class ScoreViewController {
 
-  @FXML private ListView<String> levelList;
-  @FXML private ListView<String> scoreList;
+/**
+ * This class is the designated controller for the scoreview fxml.
+ * This class contains methods to initialize the data shown on the 
+ * score screen.
+ *
+ */
+public class ScoreScreenController {
+	
+	@FXML private ListView<String> levelList;
+	@FXML private ListView<String> scoreList;
+	private List<String> levels = new ArrayList<>();
+	private List<String> scores = new ArrayList<>();
+	
+	/**
+	 * This is the entrance method used to initialize the data displayed
+	 * on the score view
+	 * @param levelData
+	 * @param scoreData
+	 */
+	public void initView(List<String> levelData, List<String> scoreData) {
+		initData(levelData, scoreData);
+		initListView(levels, levelList);
+		initListView(scores, scoreList);
+	}
+	
+	/**
+	 * This method initializes the field {@link #levels} and {@link #scores}
+	 * with data obtained from the caller method
+	 * @param levelData  the levels corresponding to the scores
+	 * @param scoreData  the scores obtained arranged in order specified
+	 * by developer
+	 */
+	private void initData(List<String> levelData, List<String> scoreData) {
+		levels.add("LEVEL");
+		scores.add("SCORE");
+	    levels.addAll(levelData);
+	    scores.addAll(scoreData);
+	}
+	
+	
+	/**
+	 * This method initializes the {@link ListView} objects with data that
+	 * is in the form of {@link String} objects
+	 * @param stringList  data in the form of a list of strings 
+	 * @param listView  the {@link ListView} to be initialized
+	 */
+	private void initListView(List<String> stringList, ListView<String> listView) {
+	    ObservableList<String> observableList = FXCollections.observableArrayList();
+	    observableList.addAll(stringList);
+	    listView.setItems(observableList);
+	    listView.setCellFactory(this::newCellFactory);
+	    
+	  }
 
-  private List<String> levels = new ArrayList<>();
-  private List<String> scores = new ArrayList<>();
+	/**
+	 * This method simulates the argument type of the calling method,
+	 * which is a {@link Callback} that takes in a {@link ListView} of
+	 * {@link String} objects and returns a {@link ListCell} of the same
+	 * {@link String} objects
+	 * @param param  this {@link ListView<String>} object
+	 * @return cell factory to use in this ListView
+	 * @see ListView#setCellFactory(Callback)
+	 * @see <a href="https://stackoverflow.com/questions/25246496/
+	 * javafx-custom-list-cell-updateitem-being-called-a-lot">Stack Overflow
+	 * question</a>
+	 */
+	private ListCell<String> newCellFactory(ListView<String>param){
+		
+		ListCell<String> cell = new ListCell<String>() {
+			@Override
+	  		public void updateItem(String item, boolean empty) {
+	  			
+	  			super.updateItem(item, empty);
+	  			
+	  			if (item==null || empty) 
+	  				setText(null);
+	  			else 
+	  				setText(item);
+		    }
+		  };
+	
+		  return cell;
+	  }
 
-  
-  public void init(GameModel gameModel) {
-    initData(gameModel);
-    initListView(levels, levelList);
-    initListView(scores, scoreList);
-  }
-
-  private void initData(GameModel gameModel) {
-    levels.add("LEVEL");
-    scores.add("SCORE");
-    levels.addAll(gameModel.getLevel());
-    scores.addAll(gameModel.getScores());
-  }
-
-  private void initListView(List<String> stringList, ListView<String> listView) {
-    ObservableList<String> observableList = FXCollections.observableArrayList();
-    observableList.addAll(stringList);
-    listView.setItems(observableList);
-    listView.setCellFactory(param -> new listViewCell());
-  }
-
-  private class listViewCell extends ListCell<String> {
-    @Override
-    protected void updateItem(String string, boolean empty) {
-      super.updateItem(string, empty);
-      if (empty || string == null) setText(null);
-      else setText(string);
-    }
-  }
 }
+
+
+//listView.setCellFactory(new Callback<ListView<String>,ListCell<String>>(){
+//
+//	@Override
+//	public ListCell<String> call(ListView<String> param) {
+//		ListCell<String> cell = new ListCell<String>() {
+//            @Override
+//            public void updateItem(String item, boolean empty) {
+//                
+//                super.updateItem(item, empty);
+//                if (empty || item == null) setText(null);
+//                else setText(item);
+//            }
+//        };
+//
+//        return cell;
+//	}
+//	
+//});
+//
+//listView.setCellFactory( (param) -> {
+//	
+//	ListCell<String> cell = new ListCell<String>() {
+//		@Override
+//		public void updateItem(String item, boolean empty) {
+//			super.updateItem(item, empty);
+//            if (empty || item == null) setText(null);
+//            else setText(item);
+//        }
+//    };
+//
+//    return cell;
+//	
+//	
+//});
