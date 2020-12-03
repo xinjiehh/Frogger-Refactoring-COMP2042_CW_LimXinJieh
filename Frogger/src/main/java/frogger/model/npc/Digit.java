963b908e31f872112cac10efa7ef9eead44cef01
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-03 21:31:21 +0800

    Docs: add Javadocs to Digit, Log and NPC

--- Frogger/src/main/java/frogger/model/npc/Digit.java
+++ Frogger/src/main/java/frogger/model/npc/Digit.java
@@ -1,23 +1,33 @@
-package frogger.model.NPC;
+package frogger.model.npc;
 
 import frogger.constant.FilePath;
+import frogger.model.Actor;
 import javafx.scene.image.Image;
 
-public class Digit extends Actor{
+/**
+ * This is the model for digit
+ *
+ */
+public class Digit extends Actor {
+	
 	private static final int WIDTH = 30;
 	private static final int HEIGHT = 30;
-	
-	
-	@Override
-	public void act(long now) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+
+
 	public Digit(int n, int x, int y) {
 		setImage(new Image( FilePath.DIGIT_PATH + n + ".png", WIDTH, HEIGHT, true, true));
 		setX(x);
 		setY(y);
 	}
+	
+	@Override
+	public void act(long now) {
+	}
+	
+	@Override
+	protected void checkOutOfBounds() {
+	}
+	
+
 	
 }
--- Frogger/src/main/java/frogger/model/npc/NPC.java
+++ Frogger/src/main/java/frogger/model/npc/NPC.java
@@ -1,10 +1,101 @@
-package frogger.model.NPC;
+package frogger.model.npc;
+
+import frogger.model.Actor;
+import frogger.model.World;
+import javafx.scene.input.InputEvent;
+
+/**
+ * This class represents all the non-playing characters
+ * which has a few properties in common 
+ */
 
 public abstract class NPC extends Actor {
+	
+	protected double speed; 
+	
+    /**
+     * @param now  the timestamp of the current frame given in nanoseconds.
+     */
+	@Override
+	public void act(long now) {
+		move(speed,0);
+		playAnimation(now);
+	}
+	
+	/**
+	 * This method allows subclasses to define their 
+	 * animation which will be called in each frame
+	 * @param now  the timestamp of the current frame given in nanoseconds.
+	 */
+	protected abstract void playAnimation(long now);
 
-	public NPC() {
+	
+	/**
+	 * This method sets the coordinates of object
+	 * @param x - x-coordinates of the object  
+	 * @param y - y-coordinates of the object
+	 */
+	
+	public void setPosition(double x, double y) {
+		setX(x);
+		setY(y);
 		
 	}
+	
+	
+	/**
+	 * This method sets the speed of object
+	 * @param s  speed of the object
+	 */
+	
+	public void setSpeed(double s) { //TODO test factory
+		this.speed = s; 
+	}
+
+	
+	/**
+	 * This method returns the speed of the object
+	 * @return speed of object
+	 */
+	
+	public double getSpeed() { 
+		return this.speed;
+	}
+	
+	/**
+	 * This method increases the speed of this character
+	 * object by a given amount 
+	 * @param increase  the integer amount of speed to 
+	 * increase
+	 */
+	public void increaseSpeed(int increase) {
+		this.speed+=increase;
+	}
+	
+    /**
+     * This method gets the value of the property parent
+     * as a {@link World} object
+     * @return parent {@link World} object
+     */
+    public World getWorld() {
+        return (World) getParent();
+    }
+    
+    /**
+     * This method returns the width of this object
+     * @return  double value of the width of this object
+     */
+    public double getWidth() {
+        return this.getBoundsInLocal().getWidth();
+    }
+
+    /**
+     * This method returns the height of this object
+     * @return  double value of the height of this object
+     */
+    public double getHeight() {
+        return this.getBoundsInLocal().getHeight();
+    }
 
 
 
