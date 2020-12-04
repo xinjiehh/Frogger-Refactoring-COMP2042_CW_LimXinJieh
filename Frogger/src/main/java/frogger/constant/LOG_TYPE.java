f1fabd42be422b32f9d5c3221a1b18ee911179f1
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-04 17:30:55 +0800

    Docs: update Javadocs for LOG_TYPE and OBSTACLE_TYPE

--- Frogger/src/main/java/frogger/constant/LOG_TYPE.java
+++ Frogger/src/main/java/frogger/constant/LOG_TYPE.java
@@ -11,20 +11,37 @@ public enum LOG_TYPE {
 	MEDIUM(FilePath.M_LOG,225),
 	SHORT(FilePath.S_LOG,150);
 	
-	
+	/** the file path for the corresponding log */
 	private String url;
+	
+	/** the size of the corresponding log */
 	private int size;
 	
+	/** 
+	 * This method returns the {@code String} that is the file path
+	 * of the media image
+	 * @return {@code String} of the image url
+	 */
 	public String getURL() {
 		return this.url;
 	}
 	
+	/**
+	 * This method returns the {@code integer} value corresponding
+	 * to the size of the log
+	 * @return  {@code integer} value of size
+	 */
 	public int getSize() {
 		return this.size;
 	}
 	
+	/**
+	 * This method is the private constructor for this enum
+	 * @param url  {@code String} corresponding to the image url
+	 * @param size  {@code integer} value corresponding to image size
+	 */
 	private LOG_TYPE (String url, int size) {
-		this.url = url;
+		this.url=url;
 		this.size=size;
 	}
 	
--- Frogger/src/main/java/frogger/constant/OBSTACLE_TYPE.java
+++ Frogger/src/main/java/frogger/constant/OBSTACLE_TYPE.java
@@ -12,19 +12,36 @@ public enum OBSTACLE_TYPE {
 	TRUCK2(FilePath.TRUCK2, 200),
 	CAR(FilePath.CAR, 50);
 	
+	/** the file path for the corresponding obstacle */
 	private String url; 
+	
+	/** the size of the corresponding obstacle */
 	private int size;
 	
-	
+	/** 
+	 * This method returns the {@code String} that is the file path
+	 * of the media image
+	 * @return {@code String} of the image url
+	 */
 	public String getURL() {
 		return this.url;
 	}
 	
+	/**
+	 * This method returns the {@code integer} value corresponding
+	 * to the size of the obstacle
+	 * @return  {@code integer} value of size
+	 */
 	public int getSize() {
 		return this.size;
 	}
 	
-	OBSTACLE_TYPE(String url, int size) {
+	/**
+	 * This method is the private constructor for this enum
+	 * @param url  {@code String} corresponding to the image url
+	 * @param size  {@code integer} value corresponding to image size
+	 */
+	private OBSTACLE_TYPE(String url, int size) {
 		this.url = url;
 		this.size = size;
 	}
