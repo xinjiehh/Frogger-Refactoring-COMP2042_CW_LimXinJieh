5538ac6d16d0392cfc171005381023cec82fb437
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-04 17:54:41 +0800

    Docs: update Javadocs

--- Frogger/src/main/java/frogger/model/npc/Digit.java
+++ Frogger/src/main/java/frogger/model/npc/Digit.java
@@ -5,7 +5,8 @@ import frogger.model.Actor;
 import javafx.scene.image.Image;
 
 /**
- * This is the model for digit
+ * This is the model for digit that is shown on the game
+ * screen
  *
  */
 public class Digit extends Actor {
@@ -13,9 +14,14 @@ public class Digit extends Actor {
 	private static final int WIDTH = 30;
 	private static final int HEIGHT = 30;
 
-
+	/**
+	 * This is the public constructor for this object
+	 * @param n  the integer value of this object
+	 * @param x  the x position for this object
+	 * @param y  the y position for this object
+	 */
 	public Digit(int n, int x, int y) {
-		setImage(new Image( FilePath.DIGIT_PATH + n + ".png", WIDTH, HEIGHT, true, true));
+		setImage(new Image(FilePath.DIGIT_PATH + n + ".png", WIDTH, HEIGHT, true, true));
 		setX(x);
 		setY(y);
 	}
