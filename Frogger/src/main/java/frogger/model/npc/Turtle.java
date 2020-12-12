416cbd0e685c75e9221cdec1c656cbfc37f84dd6
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-13 00:35:02 +0800

    Refactor: use NPCAnimation class for animations, remove if chains

--- Frogger/src/main/java/frogger/model/npc/Turtle.java
+++ Frogger/src/main/java/frogger/model/npc/Turtle.java
@@ -1,48 +1,59 @@
 package frogger.model.npc;
 
+import java.util.ArrayList;
+
 import frogger.constant.FilePath;
+import frogger.constant.RiverSprite;
+import frogger.util.animation.NPCAnimation;
 import javafx.scene.image.Image;
 
 /**
  * This class is the model for turtle 
  *
  */
-public class Turtle extends NPC {
+public class Turtle extends NPC implements RiverSprite {
 	private static final int SIZE = 130;
 	private static final Image turtle1 = new Image(FilePath.TURTLE1,SIZE,SIZE,true,true);
 	private static final Image turtle2 = new Image(FilePath.TURTLE2_DRY,SIZE,SIZE,true,true);
 	private static final Image turtle3 = new Image(FilePath.TURTLE3_DRY,SIZE,SIZE,true,true);
+	private ArrayList<Image> images = new ArrayList<Image>() {
+		{
+			add(turtle1);
+			add(turtle2);
+			add(turtle3);
+		}
+		
 	
+	};
 
 	public Turtle() {
+		super();
 		setImage(turtle2);
+		NPCAnimation anim = new NPCAnimation(this, images);
+		anim.play();
 	}
-
-
-	@Override
-	protected void checkOutOfBounds() {
-		if (getX() > 600 && speed>0)
-			setX(-200);
-		if (getX() < -75 && speed<0)
-			setX(600);
-		
-	}
-
+	
 
 	@Override
 	protected void playAnimation(long now) {
-		if (now/900000000  % 3 ==0) {
-			setImage(turtle2);
-			
-		}
-		else if (now/900000000 % 3 == 1) {
-			setImage(turtle1);
-			
-		}
-		else if (now/900000000 %3 == 2) {
-			setImage(turtle3);
-			
-		}
+//		if (now/900000000  % 3 ==0) {
+//			setImage(turtle2);
+//			
+//		}
+//		else if (now/900000000 % 3 == 1) {
+//			setImage(turtle1);
+//			
+//		}
+//		else if (now/900000000 %3 == 2) {
+//			setImage(turtle3);
+//			
+//		}
+	}
+
+
+	@Override
+	public NPC clone() {
+		return new Turtle();
 	}
 
 
--- Frogger/src/main/java/frogger/model/npc/WetTurtle.java
+++ Frogger/src/main/java/frogger/model/npc/WetTurtle.java
@@ -1,64 +1,86 @@
 package frogger.model.npc;
 
+import java.util.ArrayList;
+
 import frogger.constant.FilePath;
+import frogger.constant.RiverSprite;
+import frogger.util.CollisionDetector;
+import frogger.util.animation.NPCAnimation;
+import frogger.util.animation.SpriteAnimationTemplate;
 import javafx.scene.image.Image;
 
 /**
- * This class is the model for wet turtle
+ * This class is the model for wet turtle, which implements {@link 
+ * Sinkable} that has the property {@code sunk} that is useful in 
+ * {@link CollisionDetector} to easily determine if an {@link NPC} 
+ * is sunken. There is also an option to define animation without
+ * using {@link SpriteAnimationTemplate} using the {@link #playAnimation()}
+ * which changes the {@code Image} of this object based on the time
+ * frame passed
  *
  */
-public class WetTurtle extends NPC {
+public class WetTurtle extends NPC implements Sinkable, RiverSprite {
+	
 	private static final int SIZE = 130;
 	private static final Image turtle1 = new Image(FilePath.TURTLE1,SIZE,SIZE,true,true);
 	private static final Image turtle2 = new Image(FilePath.TURTLE2_WET,SIZE,SIZE,true,true);
 	private static final Image turtle3 = new Image(FilePath.TURTLE3_WET,SIZE,SIZE,true,true);
 	private static final Image turtle4 = new Image(FilePath.TURTLE4_WET,SIZE,SIZE,true,true);
-	private boolean sunk = false;
+	private boolean isSunk;
+	ArrayList<Image> images = new ArrayList<Image>() {
+		{
+			add(turtle1);
+			add(turtle2);
+			add(turtle3);
+			add(turtle4);
+		}
+
+	};
 
 	public WetTurtle() {
+		super();
 		setImage(turtle2);
+		NPCAnimation anim = new NPCAnimation(this, images);
+		anim.play();
 	}
 
+	@Override
 	public boolean isSunk() {
-		return sunk;
+		return isSunk;
 	}
-	
-	
-	@Override
-	protected void checkOutOfBounds() {
-		if (getX() > 600 && speed>0)
-			setX(-200);
-		
-		if (getX() < -75 && speed<0)
-			setX(600);
-	}
-	
-	@Override
-	protected void playAnimation(long now) {
-		if (now/900000000  % 4 ==0) {
-			setImage(turtle2);
-			sunk = false;
-		}
-		
-		else if (now/900000000 % 4 == 1) {
-			setImage(turtle1);
-			sunk = false;
-		}
-		
-		else if (now/900000000 %4 == 2) {
-			setImage(turtle3);
-			sunk = false;
-		} 
-		
-		
-		else if (now/900000000 %4 == 3) {
-			setImage(turtle4);
-			sunk = true;
-		}
-		
-	}
-	
 
+
+	@Override
+	public void setSunk(boolean b) {
+		isSunk = b;
+		
+	}
+
+	@Override
+	public NPC clone() {
+		return new WetTurtle();
+	}
+	
+//	if (now/900000000  % 4 ==0) {
+//	setImage(turtle2);
+//	sunk = false;
+//}
+//
+//else if (now/900000000 % 4 == 1) {
+//	setImage(turtle1);
+//	sunk = false;
+//}
+//
+//else if (now/900000000 %4 == 2) {
+//	setImage(turtle3);
+//	sunk = false;
+//} 
+//
+//else if (now/900000000 %4 == 3) {
+//	setImage(turtle4);
+//	sunk = true;
+//}
+	
 //	
 //	private Image newTurtle(String link) {
 //		
