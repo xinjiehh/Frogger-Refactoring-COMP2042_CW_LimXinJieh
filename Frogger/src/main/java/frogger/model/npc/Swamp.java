efb1a1d02dbf52fcaa692de1a14b527b874a50ba
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-13 00:34:05 +0800

    Refactor: use Transition for animations, remove if chains

--- Frogger/src/main/java/frogger/model/npc/Swamp.java
+++ Frogger/src/main/java/frogger/model/npc/Swamp.java
@@ -1,183 +1,114 @@
 package frogger.model.npc;
 
+import java.util.ArrayList;
 import java.util.Random;
+
 import frogger.constant.FilePath;
+import frogger.util.animation.SpriteAnimationTemplate;
+import javafx.animation.Transition;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 
 
 /**
  * This class is the model for Swamp. The Swamp object image will alternate
- * to show crocodiles or flies, and changes when a frog approaches a swamp spot.
+ * to show crocodiles or flies, and changes when a frog approaches a {@code
+ * Swamp} object
  */
 
 public class Swamp extends NPC {
 	
 	private static final int WIDTH = 65;
 	private static final int HEIGHT = 60;
-	private static final Image FLY_END = new Image(FilePath.S_FLY, WIDTH, HEIGHT, false, true);
 	private static final Image END = new Image(FilePath.SWAMP, WIDTH, HEIGHT, false, true);
-	private static final Image CROC_END2 = new Image(FilePath.S_CROC1, WIDTH, HEIGHT, false, true);
-	private static final Image CROC_END1 = new Image(FilePath.S_CROC2, WIDTH, HEIGHT, false, true);
 	private static final Image FROG_END = new Image(FilePath.S_FROG, WIDTH, HEIGHT, false, true);
-	private static int DELAY = 10;
+	
 	private static int MAX_FLY = 1;
 	private static int MAX_CROC = 2;
 	private static int flyCtr = 0;
 	private static int crocCtr = 0;
+	
+	private int x;
+	private int y;
+	private boolean noAnimation = true;
 	private boolean hasFly = false;
 	private boolean hasCroc = false;
 	private boolean activated = false;
-	private int crocAnimCtr=0;
-	private int flyAnimCtr=0;
+	private FlyAnimation flyAnim = new FlyAnimation();
+	private CrocAnimation crocAnim = new CrocAnimation();
+
+	
+
+	/**
+	 * This method is a public constructor to create a {@code Swamp} object
+	 * @param x  x position of object
+	 * @param y  y position of object
+	 */
+	public Swamp(int x, int y) {
+		super();
+		setImage(END);
+		initPos(x,y);
+		
+	}
 	
 	/**
-	 * This method checks if swamp is empty, then randomly 
-	 * generates a crocodile or fly in its spot
+	 * This method checks if {@code Swamp} is empty, then randomly 
+	 * generates a crocodile or fly in its spot using animation methods
+	 * {@code playCrocAnim()} and {@code playFlyAnim()}
 	 */
 	@Override
 	public void act(long now) {
-
-		if(!activated) {
 		
+		if(!activated) {
+			
 			if(!hasFly && !hasCroc) {
 				
 				int rand = new Random().nextInt(100);
 
 				if(rand==98 && flyCtr<MAX_FLY){
 					hasFly=true;
-					System.out.println("fly ctr is " + flyCtr);
 					flyCtr+=1;
 					
 				} else if (rand==99 && crocCtr<MAX_CROC) {
-					crocCtr+=1;
 					hasCroc=true;
-					
+					crocCtr+=1;
 				}
 				
 			}
 			
-			if(hasCroc) {
-				handleCroc(now);
-			}
-
-			
-			if(hasFly) {
-				handleFly(now);
-			}
-
-		}
-	
-	}
-
-
-	/**
-	 * This method handles appearance and disappearance of fly in
-	 * the swamp. The fly is set to disappear after a fixed time.
-	 * @param now  the timestamp of the current timeframe given 
-	 * in nanoseconds	 
-	 */
-	
-	private void handleFly(long now) {
-		
-		if(activated) {
-			flyCtr-=1;
-			
-		} else {
-			
-			if(now%11==0) {
-				this.flyAnimCtr++;
+			if(hasCroc && noAnimation) {
+				playCrocAnim();
 			}
 			
-			if(flyAnimCtr==1) {
-				setImage(FLY_END);
+			if(hasFly && noAnimation) {
+				playFlyAnim();
 				
 			}
-			
-			if(flyAnimCtr==15) {
-				setImage(END);
-				hasFly=false;
-				flyCtr-=1;
-				this.flyAnimCtr=0;
-				
-			
-		}
-		
-		
-			
-			
-		}
 
-	}
+		} 
 	
+	}
+
 
 	/**
-	 * This method handles appearance and disappearance of crocodile in 
-	 * the swamp. The crocodile is set to disappear after a fixed time.
-	 * @param now  the timestamp of the current timeframe given 
-	 * in nanoseconds
-	 */
-	private void handleCroc(long now) {
-
-		if(now%11==0) {
-			this.crocAnimCtr++;
-		}
-		
-		crocAnim();
-	}
-	
-	
-	 /**
-	  * This method handles the appearing and disappearing
-	  * animation of the crocodile at the swamp.  
-	  */
-
-	private void crocAnim() {
-		if(this.crocAnimCtr==1 || this.crocAnimCtr==2+DELAY) {
-			setImage(CROC_END2);
-
-		} else if(this.crocAnimCtr==2) {
-			setImage(CROC_END1);
-			
-		} else if(this.crocAnimCtr==4+DELAY) {
-			setImage(END);
-			hasCroc=false;
-			crocCtr-=1;
-			this.crocAnimCtr=0;
-		}
-	}
-
-	/**
-	 * This method resets this object to an empty swamp image
+	 * This method resets this object to an empty image
 	 */
 	public void resetEnd() {
 		setImage(END);
 	}
 	
 	/**
-	 * This method resets the counter for flies and crocodiles
+	 * This method resets the {@code flyCtr} and {@code crocCtr} that is
+	 * shared by ALL {@link Swamp} objects
 	 */
 	public static void resetCtr() {
 		flyCtr=0;
 		crocCtr=0;
 	}
 
-
 	/**
-	 * This method is a public constructor to create an End object
-	 * @param x  x position of object
-	 * @param y  y position of object
-	 */
-	public Swamp(int x, int y) {
-		setX(x);
-		setY(y);
-		setImage(END);
-	}
-	
-
-
-	/**
-	 * This method changes the End object Image to show an 'occupied' swamp
-	 * spot
+	 * This method changes the {@link Swamp} object Image to show an 
+	 * 'occupied' ({@code activated} = true) spot
 	 */
 	public void setOccupied() {
 		setImage(FROG_END);
@@ -186,16 +117,17 @@ public class Swamp extends NPC {
 	
 
 	/**
-	 * This method checks if the End object is previously activated / 
+	 * This method checks if the {@link Swamp} object is {@code activated} / 
 	 * occupied by a frog
-	 * @return boolean variable true if this object is 'occupied', false otherwise
+	 * @return boolean variable true if this object is 'occupied', false 
+	 * otherwise
 	 */
 	public boolean isActivated() {
 		return activated;
 	}
 	
 	/**
-	 * 
+	 * This method returns true if this {@link Swamp} object has a fly
 	 * @return boolean variable true if this object has a fly, false
 	 * otherwise
 	 */
@@ -204,7 +136,7 @@ public class Swamp extends NPC {
 	}
 	
 	/**
-	 * 
+	 * This method returns true if this {@link Swamp} object has a crocodile
 	 * @return boolean variable true if this object has a crocodile,
 	 * false otherwise
 	 */
@@ -212,92 +144,308 @@ public class Swamp extends NPC {
 	public boolean hasCroc() {
 		return hasCroc;
 	}
-	
-	
 
 
 	@Override
-	protected void checkOutOfBounds() {
-		
+	public NPC clone() {
+		return new Swamp(this.x,this.y);
 	}
-
-
-	@Override
-	protected void playAnimation(long now) {
-		
+	
+	/**
+	 * This method initializes the position of this {@link Swamp} object
+	 * @param x  integer x position
+	 * @param y  integer y position
+	 */
+	private void initPos(int x, int y) {
+		this.x=x;
+		this.y=y;
+		setX(x);
+		setY(y);
 		
 	}
 	
+	/**
+	 * This method plays the fly animation and sets the relevant
+	 * flags 
+	 */
+	private void playFlyAnim() {
+		flyAnim.play();
+		noAnimation = false;
+		hasFly=true;
+	}
+	
+	/**
+	 * This method plays the crocodile animation and sets the relevant
+	 * flags 
+	 */
+	private void playCrocAnim() {
+		crocAnim.play();
+		noAnimation = false;
+		hasCroc=true;
+	}
+	
+	/**
+	 * This class is responsible for animating the fly in this {@link Swamp}
+	 * object
+	 */
+	private class FlyAnimation extends SpriteAnimationTemplate {
+		
+		private final Image FLY_END = new Image(FilePath.S_FLY, WIDTH, HEIGHT, false, true);
+		
+		@Override
+		protected void initAnimation() {
+			animation = new Transition() {
+			{
+				setCycleDuration(Duration.millis(3000));
+			}
+
+				@Override
+				protected void interpolate(double frac) {
+					setImage(FLY_END);
+					if(activated) {
+						setImage(FROG_END);
+						stop();
+						endAnimation();
+					}
+				}
+			};
+			
+			animation.setOnFinished(e->{
+				setImage(END);
+				endAnimation();
+			});
+			
+		}
+
+		protected void endAnimation() {
+			flyCtr-=1;
+			hasFly=false;
+			donePlaying=true;
+			noAnimation=true;
+			
+		}
+		
+	}
+
+	/**
+	 * This class is responsible for animating the crocodile in this {@link 
+	 * Swamp} object
+	 */
+	private class CrocAnimation extends SpriteAnimationTemplate {
+		private final Image CROC_END2 = new Image(FilePath.S_CROC2, WIDTH, HEIGHT, false, true);
+		private final Image CROC_END1 = new Image(FilePath.S_CROC1, WIDTH, HEIGHT, false, true);
+		private ArrayList<Image> crocImages = new ArrayList<Image>() {
+			{
+				add(CROC_END2);
+				add(CROC_END1);
+				add(CROC_END2);
+				add(CROC_END2);
+			}
+
+		};
+
+		@Override
+		protected void initAnimation() {
+			animation = new Transition() {
+				{
+					setCycleDuration(Duration.millis(3000));
+				}
+
+				@Override
+				protected void interpolate(double frac) {
+					int index = (int) (frac*(crocImages.size()-1));
+			        index = index > crocImages.size()-1 ? crocImages.size()-1 : index;
+			        setImage(crocImages.get(index)); 
+					
+				}
+			};
+			
+			animation.setOnFinished(e->{
+				setImage(END);
+				crocCtr-=1;
+				hasCroc=false;
+				donePlaying=true;
+				noAnimation=true;
+				
+			});
+			
+		}
+		
+	}
 
 }
-//private void createTimerTask() {
-//	
-//	task2 = new TimerTask() {
+
+//private void initAnim() {
+	//initFlyAnim();
+	//initCrocAnim();
+//}
+
+
+//private void initCrocAnim() {
+	
+//	crocAnim = new Transition() {
+//		{
+//			setCycleDuration(Duration.millis(3000));
+//		}
 //
 //		@Override
-//		public void run() {
-//			if(!activated) {
-//				
-//				if(hasFly) {
-//					hasFly=false;
-//					flyCtr-=1;
-//				} 
-//				setEndImage("End.png");
+//		protected void interpolate(double frac) {
+//			int index = (int) (frac*(crocImages.size()-1));
+//	        index = index > crocImages.size()-1 ? crocImages.size()-1 : index;
+//	        System.out.println(index);
+//	        setImage(crocImages.get(index)); 
+//			
+//		}
+//	};
+//	
+//	crocAnim.setOnFinished(e->{
+//		setImage(END);
+//		crocCtr-=1;
+//		hasCroc=false;
+//		donePlaying=true;
+//	});
+//}
+
+
+
+
+//private void initFlyAnim() {
+//	
+//	flyAnim = new FlyAnimation();
+//	flyAnim = new Transition() {
+//		{
+//			setCycleDuration(Duration.millis(3000));
+//		}
+//
+//		@Override
+//		protected void interpolate(double frac) {
+//			setImage(FLY_END);
+//			if(activated) {
+//				setImage(FROG_END);
+//				flyAnim.stop();
+//				flyCtr-=1;
+//				hasFly=false;
+//				donePlaying=true;
 //				
 //			}
-//			
+//				
+//				
+//		}
+//	};
+//		
+//	flyAnim.setOnFinished(e->{
+//		setImage(END);
+//		flyCtr-=1;
+//		donePlaying=true;
+//		hasFly=false;
+//	});
+//		
+//}
+
+
+
+///**
+// * This method handles appearance and disappearance of fly in
+// * the swamp. The fly is set to disappear after a fixed time.
+// * @param now  the timestamp of the current timeframe given 
+// * in nanoseconds	 
+// */
+//
+//private void handleFly(long now) {
+//	
+//	//if player caught fly
+//	if(activated) {
+//		flyCtr-=1;
+//		
+//	} else {
+//		
+//		if(now%11==0) {
+//			this.flyAnimCtr++;
+//		}
+//		
+//		if(flyAnimCtr==1) {
+//			setImage(FLY_END);
 //			
 //		}
 //		
-//	};
-//
-//	
-//}
-
-//if (GameViewManager.pause) {
-//this.pause=true;
-//flyTimer.cancel();
-//crocTimer.cancel();
-//System.out.println("fly timer cancelled");
-//
-//} else {
-//resume();
-//}
-//
-
-//private void resume() {
-//	if(this.pause) {
-//		this.flyTimer = new Timer();
-//		this.flyTimer.schedule(task2,0,1000);
+//		if(flyAnimCtr==flyDuration) {
+//			setImage(END);
+//			hasFly=false;
+//			flyCtr-=1;
+//			this.flyAnimCtr=0;
+//		}
+//		
 //	}
-//	// TODO Auto-generated method stub
-//	
+//
 //}
 //
-//private void handleFly() {
-//	handleFly=false;
-//	hasFly=true;
-//	setEndImage("FlyEnd.png");
-//	flyCtr+=1;
-//	createTimerTask();
-//	flyTimer.schedule(task2, 2000);
-//	System.out.println("fly timer created");
-//}
-
+//
+///**
+// * This method handles appearance and disappearance of crocodile in 
+// * the swamp. The crocodile is set to disappear after a fixed time.
+// * @param now  the timestamp of the current timeframe given 
+// * in nanoseconds
+// */
 //private void handleCroc(long now) {
+//
 //	if(now%11==0) {
-//		crocAnimCtr++;
+//		this.crocAnimCtr++;
 //	}
 //	
-//	if(crocAnimCtr==1) {
-//		setEndImage("CrocEnd2.png");
-//		crocCtr+=1;
-//		createTimerTask();
-//		crocTimer.schedule(task1, 4500);
-//		crocTimer.schedule(task2, 5000);
-//		
-//	} else if(crocAnimCtr==2) {
-//		setEndImage("CrocEnd1.png");
-//		
-//	} 
+//	crocAnim();
 //}
+
+///**
+// * This method handles appearance and disappearance of fly in
+// * the swamp. The fly is set to disappear after a fixed time.
+// * @param now  the timestamp of the current timeframe given 
+// * in nanoseconds	 
+// */
+//
+//private void handleFly(long now) {
+//	
+//	//if player caught fly
+//	if(activated) {
+//		flyCtr-=1;
+//		
+//	} else {
+//		
+//		if(now%11==0) {
+//			this.flyAnimCtr++;
+//		}
+//		
+//		if(flyAnimCtr==1) {
+//			setImage(FLY_END);
+//			
+//		}
+//		
+//		if(flyAnimCtr==flyDuration) {
+//			setImage(END);
+//			hasFly=false;
+//			flyCtr-=1;
+//			this.flyAnimCtr=0;
+//		}
+//		
+//	}
+//
+//}
+//
+//
+///**
+// * This method handles appearance and disappearance of crocodile in 
+// * the swamp. The crocodile is set to disappear after a fixed time.
+// * @param now  the timestamp of the current timeframe given 
+// * in nanoseconds
+// */
+//private void handleCroc(long now) {
+//
+//	if(now%11==0) {
+//		this.crocAnimCtr++;
+//	}
+//	
+//	crocAnim();
+//}
+
+
+
+
