7a84d1e23536a1fa53f534ea400c8d489a350d76
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-13 13:39:58 +0800

    Refactor: new methods to add, remove and switch Scenes in a HashMap

--- Frogger/src/main/java/frogger/controller/ScreenController.java
+++ Frogger/src/main/java/frogger/controller/ScreenController.java
@@ -1,33 +1,73 @@
 package frogger.controller;
 
 import frogger.Main;
+import frogger.controller.SelectionController.Controls;
 import frogger.view.MenuScreen;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import frogger.controller.SelectionController.Controls;
+
+import java.util.HashMap;
 
 /**
- * This class follows singleton method using enum 
- *
+ * This class is the entry class from {@link Main}, responsible
+ * for switching between different views on the main stage  
+ * This class follows singleton method using enum. 
  */
 public enum ScreenController {
 	INSTANCE;
+	
+	private HashMap<String,Pane> screenMap = new HashMap<>();
+	private final Stage stage = Main.getPrimaryStage();
+	private final Scene scene = stage.getScene();
+	
+	
+	/**
+	 * This method allows to easily add new views to be displayed
+	 * on the main stage
+	 * @param id  {@code String} to act as screen identifier
+	 * @param pane  {@code Pane} object of the screen
+	 */
+	public void addScreen(String id, Pane pane) {
+		screenMap.put(id, pane);
 
+	}
+	
+
+	/**
+	 * This method allows to easily remove views from the stage
+	 * @param name  {@code String} to act as screen id
+	 */
+	public void removeScreen(String name) {
+		screenMap.remove(name);
+		
+	}
+	
+	/**
+	 * This method allows easy switching of the view 
+	 * using the identifier {@code String} to get the
+	 * corresponding {@code Pane}. The {@code Pane} is 
+	 * set as the root of this {@link #scene} object 
+	 * 
+	 * @param name  {@code String} identifier of the {@code Pane}
+	 * to be displayed
+	 */
+	public void activate(String name) {
+		scene.setRoot(screenMap.get(name));
+	}
+	
 	/**
 	 * This method initializes the {@code Stage} with the menu screen 
 	 * ({@code Pane} object from {@link MenuScreen}) and starts the 
 	 * menu screen animation before showing the stage
 	 */
 	public void showMenu() {
-		Stage stage = Main.getPrimaryStage();
 		MenuScreen menu = MenuScreen.getInstance();
-		
-		menu.startAnim();
-		stage.getScene().setRoot(menu.getPane());
-		stage.show();
-		
+		scene.setRoot(menu.getPane());
+		stage.show();	
 	}
+
+
 
 	/**
 	 * This method starts the game and passes on the controls
