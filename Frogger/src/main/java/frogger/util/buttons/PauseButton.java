3871b9a3cab9fb103c646c0470744e1f5ff0dc94
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-13 13:51:38 +0800

    Docs: update Javadocs

--- Frogger/src/main/java/frogger/util/buttons/PauseButton.java
+++ Frogger/src/main/java/frogger/util/buttons/PauseButton.java
@@ -2,19 +2,17 @@ package frogger.util.buttons;
 
 
 import frogger.constant.FilePath;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
 /**
- * constructor initializes fixed button width, height, font, with customizable text
- * removed xPos yPos from constructor
+ * This class defines {@code Button} object used in {@link GameScreen}
+ * for pausing/playing the game
+ *
  */
-//<%=outterBoxWidth;%>
-
-public class PauseButton extends Button implements CustomButton {
+public class PauseButton extends Button {
 
 	private static final String BUTTON_STYLE = "-fx-background-color: transparent;" + 
 			   "-fx-background-size: cover;" + 
@@ -23,32 +21,36 @@ public class PauseButton extends Button implements CustomButton {
 	private static final String PLAY_BUTTON = BUTTON_STYLE + "right.png" + "\");";
 	private static final String PAUSE_BUTTON = BUTTON_STYLE + "pause.png" + "\");";
 	
-	public enum BUTTON {
-		EXIT,
-		PAUSE;
-	}
-	
-	
+	/**
+	 * This public constructor initalizes the id, preferred width, height,
+	 * style and listeners of this {@code ExitButton} object
+	 */
 	public PauseButton () {
 		setPrefWidth(60);
 		setPrefHeight(60);
 		setStyle(PAUSE_BUTTON);
 		setId("pause");
-		initialiseButtonListeners(); 
+		initListeners(); 
 		
 	}
 		
 
 	
-	@Override
+	/**
+	 * This method sets the preferred height and layout 
+	 * for a pressed button
+	 */
 	public void setButtonPressedStyle() {
 		setPrefHeight(55);
 		setLayoutY(getLayoutY()+4);
 		
 	}
 
-
-	@Override
+	/**
+	 * This method switches the style and id of this {@code Button}
+	 * object according to its previous id (pause/play), and sets the 
+	 * preferred height and layout for a released button
+	 */
 	public void setButtonReleasedStyle() {
 		setStyle(getId() == "pause"? PLAY_BUTTON : PAUSE_BUTTON);
 		setId(getId() == "pause"? "play" : "pause");
@@ -57,9 +59,12 @@ public class PauseButton extends Button implements CustomButton {
 		
 	}
 
-
-	@Override
-	public void initialiseButtonListeners() {
+	/**
+	 * This method registers event handlers for mouse events by defining 
+	 * methods to handle mouse presence (entrance / exit) and mouse press 
+	 * (pressed / released) on this object.
+	 */
+	public void initListeners() {
 		setOnMousePressed(this::handleMouseEvent);
 		setOnMouseReleased(this::handleMouseEvent);
 		setOnMouseExited(this::handleMouseEvent);
@@ -73,7 +78,7 @@ public class PauseButton extends Button implements CustomButton {
 	 * (pressed / released) on this object.
 	 * @param e mouse event to be handled
 	 */
-	@Override
+
 	public void handleMouseEvent(MouseEvent e) {
 		if(e.getButton().equals(MouseButton.PRIMARY) || e.getButton().equals(MouseButton.NONE)) {
 
