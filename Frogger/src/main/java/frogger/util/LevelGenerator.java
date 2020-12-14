c16e5787fefc07e710320309616c817e3b91b513
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-14 18:43:33 +0800

    Refactor: create LevelGenerator to define algorithm for level generation

---
+++ Frogger/src/main/java/frogger/util/LevelGenerator.java
@@ -0,0 +1,63 @@
+package frogger.util;
+
+import frogger.model.Lane;
+import frogger.model.LevelTemplate;
+
+/**
+ * This class contains a fixed algorithm used to generate levels
+ *
+ */
+public class LevelGenerator extends LevelTemplate {
+
+
+	private int level;
+	private double speed = 0;
+	private int raceCar = 1;
+	private int longLog = 2;
+	private int turtle5 = 4;
+	private int turtle8 = 3;
+	
+	public LevelGenerator(int level) {
+		super();
+		this.level = level;
+		initializeLanes();
+		
+
+	}
+	
+	/**
+	 * This method initialize the each lane with different
+	 * number and speed of elements. Element type is fixed
+	 * for each lane. However, you can easily change this in
+	 * {@link Lane} class.
+	 * 
+	 * increase speed for even number levels
+	 * for 4,8    : increase number of race cars
+	 * for 3,6,9  : decrease number of turtles
+	 * for 5,7    : decrease number of turtles
+	 * 
+	 */
+	@Override
+	public void initializeLanes() {
+		
+		speed += (level%2==0) ? 0.3 : 0;
+		turtle5 -= (level%4==0) ? 1 : 0;
+		raceCar += (level%3==0) ? 1 : 0; 
+		turtle8 -= (level==5 || level == 7) ? 1 : 0;
+		
+
+		addLane(1).initLaneElements(3,1+speed); //adds to gameElements list
+		addLane(2).initLaneElements(4,-1+speed);
+		addLane(3).initLaneElements(2,1+speed);
+		addLane(4).initLaneElements(raceCar,5+speed);
+		addLane(5).initLaneElements(turtle5,-1+speed);
+		addLane(6).initLaneElements(3,0.75+speed);
+		addLane(7).initLaneElements(longLog,-2+speed);
+		addLane(8).initLaneElements(turtle8,-2+speed);
+		addLane(9).initLaneElements(3,0.75+speed);
+
+	}
+
+
+}
+
