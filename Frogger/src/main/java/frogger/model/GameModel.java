ea52976d9ceaa5938258a4ef8ce62993946aa68c
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-14 18:49:04 +0800

    Refactor: implement Subject interface, optimize methods

--- Frogger/src/main/java/frogger/model/GameModel.java
+++ Frogger/src/main/java/frogger/model/GameModel.java
@@ -1,32 +1,31 @@
 package frogger.model;
 
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import frogger.constant.GameOver;
+import frogger.constant.EndGame;
+import frogger.constant.Settings;
 import frogger.controller.GameController;
 import frogger.model.npc.Digit;
+import frogger.model.npc.Swamp;
 import frogger.util.GameGenerator;
-import javafx.animation.AnimationTimer;
+import frogger.util.animation.SpriteAnimationTemplate;
 import javafx.scene.Node;
 
+import java.util.*;
 
 
 /**
  * This class implements the MVC pattern. It is the Model for GameScreen 
- *  
+ * containing all the data (game state etc) and logic (handling game over
+ * etc). This class also follows the Observer pattern by implementing the
+ * Subject interface. Any updates to the {@link #isPaused} will notify the
+ * subscribers (in this case, classes implementing {@link SpriteAnimationTemplate})
+ * to pause/play {@code Transition} accordingly
  *
  */
 
-public class GameModel {
+public class GameModel implements Subject {
 	
 	private static final int SCORE_X = 550; 
 	private static final int SCORE_Y = 10;
-	private static final int MAX_LEVEL = 10;
 	
 	/** list of played levels and their corresponding scores stored in Map as key-value pair */
 	private static List<Map.Entry<Integer, Integer>> levelScoreList = new ArrayList<>();
@@ -38,45 +37,44 @@ public class GameModel {
 	private List<String> scores = new ArrayList<String>();
 	
 	/** current level */
-	private int levelNum; 
+	private int levelNum = 0; 
 	
 	/** current player character */
-	private Frog frog; 
+	private PlayerAvatar player; 
 	
 	/** array of score digits to add to view */
 	private LinkedList<Node> scoreDigit = new LinkedList<Node>(); 
-	
-	/** index for animation */
-	private int animCtr = 0;
 	
 	/** list of all the characters / game elements to be displayed on view */
 	private List<Node> elementList;
 	
 	/** the state of the game */
-	private GameOver state;
+	private EndGame state;
 	
 	/** used to generate the characters / game elements */
 	private GameGenerator generator;
 	
-
-	/** flag if bonus is played */
-	private boolean hasBonus = false;
+	private String scoreString = "";
 	
-	/** timer responsible for playing the bonus animation */
-	private AnimationTimer bonusTimer;
+	private boolean isPaused = false;
 	
 	/**
 	 * This is the public constructor for this object
 	 * @param level  the current level
 	 */
-	public GameModel(int level) { 
-		
-		this.levelNum = level;		
+	public GameModel() { 
+		addEvent("sprite","pause","level");
+		//replaced by Observer pattern
+		//GameController.INSTANCE.addToView(elementList); 
+
+	}
+	
+	public void newLevel() {
+		this.levelNum+=1;
+		notify("level",this);
+		Swamp.resetCtr();
 		initElements();
-		createBonusTimer();
-		setScore(frog.getScore());
-		GameController.INSTANCE.addToView(elementList);
-		
+		System.out.println("This is level "+ levelNum);
 	}
 
 	
@@ -89,73 +87,90 @@ public class GameModel {
 	}
 	
 	/**
-	 * This method returns the {@link Frog} object used in this 
+	 * This method returns the {@link PlayerAvatar} object used in this 
 	 * {@link GameModel} object
-	 * @return  the {@link Frog} object
+	 * @return  the {@link PlayerAvatar} object
 	 */
-	public Frog getFrog() {
-		return this.frog;
+	public PlayerAvatar getPlayer() {
+		return this.player;
 	}
 	
 	/**
-	 * This method returns the {@link GameOver} state of the
+	 * This method returns the {@link EndGame} state of the
 	 * game
 	 * 
-	 * @return  {@link GameOver#WIN} when all levels are completed,
-	 * {@link GameOver#LOSE} when all lives are lost, {@link GameOver#NEXT} 
-	 * when all frogs reach the swamp 
+	 * @return  {@link EndGame#WIN} when all levels are completed,
+	 * {@link EndGame#LOSE} when all lives are lost, {@link EndGame#NEXT} 
+	 * when all {@code PlayerAvatar} reach the swamp 
 	 */
-	public GameOver getState() {
+	public EndGame getState() {
 		return state;
 	}
 	
 	/**
 	 * This method handles the actions to be taken when 
 	 * <u1> 
-	 * <li> all five Frogs have reached the swamp, or</li>
-	 * <li> the Frog object dies and loses all its lives</li>
+	 * <li> all five {@code PlayerAvatar} have reached the swamp, or</li>
+	 * <li> the {@code PlayerAvatar} object dies and loses all its lives</li>
 	 * </u1>
 	 * <p>
 	 * 
-	 * @param state {@link GameOver#LOSE} when all lives are lost, {@link GameOver#NEXT} 
-	 * when all frogs reach the swamp 
+	 * @param state {@link EndGame#LOSE} when all lives are lost, {@link EndGame#NEXT} 
+	 * when all {@code PlayerAvatar} reach the swamp 
 	 */
-	public void handleDoneLevel(GameOver state) {
-		frog.setNoMove(true);
+	public void handleDoneLevel(EndGame state) {
+		player.setNoMove(true);
 		updateScoreList();
 		pauseAllTimer();
-		this.state = (state==GameOver.LOSE) ? GameOver.LOSE : 
-			(levelNum<MAX_LEVEL) ? GameOver.NEXT : GameOver.WIN;
-	}
-	
-	/**
-	 * This method starts the {@link #bonusTimer} and 
-	 * sets {@link #hasBonus} to true
-	 */
-	public void playBonus() {
-		hasBonus=true;
-		bonusTimer.start();
-	}
-	
-	
-	/**
-	 * This method stops all unfinished timers
-	 */
-	public void pauseAllTimer() {
-		if(hasBonus)
-			bonusTimer.stop();
-	}
-	
-	
-	/**
-	 * This method starts all unfinished timers
-	 */
-	public void continueAllTimer() {
-		if(hasBonus) 
-			bonusTimer.start();
 		
+		if(state==EndGame.LOSE) {
+			this.state=state;
+			resetGame();
+			
+		} else {
+			this.state = 
+				(levelNum<Settings.MAX_LEVEL) ? EndGame.NEXT : 
+					EndGame.WIN;
+			//new HighScoreFile(scoreString);
+		}
+	}
+	
+	public String getScoreString() {
+		return scoreString;
 	}
 
+	/**
+	 * This method stops all timers/animations
+	 */
+	public void pauseAllTimer() {
+		isPaused = true;
+		notify("pause", this);
+	}
+	
+	public int getLevel() {
+		return levelNum;
+	}
+
+	
+	/**
+	 * This method starts all unfinished timers/animations
+	 */
+	public void continueAllTimer() {
+		isPaused = false;
+		notify("pause", this);
+		
+	}
+	
+	/**
+	 * This method returns property {@code isPaused} which 
+	 * determines if the game is playing/paused
+	 * 
+	 * @return  true if the game is paused
+	 */
+	public boolean getIsPaused() {
+		return isPaused;
+	}
+	
 
 	
 	/**
@@ -164,15 +179,14 @@ public class GameModel {
 	 * @return  String to notify users of the next action
 	 */
 	public String getProgressMessage() {
-		String msg = (state==GameOver.LOSE)? "Game Over" : 
-			(levelNum<MAX_LEVEL)? "Next level!" : 
+		return (state==EndGame.LOSE)? "Game Over" :
+			(levelNum < Settings.MAX_LEVEL)? "Next level!" :
 			"Congratulations, you won the game!";
-		return msg;
 		
 	}
 	
 	/**
-	 * This method returns the list of scores arranged indescending 
+	 * This method returns the list of scores arranged in descending
 	 * order
 	 * 
 	 * @return  {@link List} of scores as {@code String} objects
@@ -186,15 +200,19 @@ public class GameModel {
 	 * order of scores
 	 * @return  {@link List} of levels as {@code String} objects
 	 */
-	public List<String> getLevel(){
+	public List<String> getLevelList(){
 		return levels;
 	}
 	
 
 	/**
-	 * This method clears the score list {@link #levelScoreList}
+	 * This method resets the game data by clearing the score 
+	 * list {@link #levelScoreList}, reset {@link Swamp} counters
+	 * and set level number to 0
 	 */
-	public void resetScoreList() {
+	public void resetGame() {
+		Swamp.resetCtr();
+		levelNum=0;
 		levelScoreList.clear();
 	}
 
@@ -221,27 +239,29 @@ public class GameModel {
 		} else {
 			
 			int xShift = 0; 
-			int number = score; //TODO added because dont want to modify params bad programming practice
+			int number = score; 
 			while (number > 0) {
-				int ones = number % 10;  //ones = points - quotient * 10; TODO changed
+				int ones = number % 10; 
 				scoreDigit.add(new Digit(ones, SCORE_X - xShift, SCORE_Y));
 				number/=10;
 				xShift+=30; //shift digit to the left with each while loop
 			}
 		}
-		//add score array to view
+		
 		GameController.INSTANCE.addToView(scoreDigit);
 
 	}
 	
 	/**
-	 * This method initializes {@link #generator}, {@link #frog} and 
+	 * This method initializes {@link #generator}, {@link #player} and 
 	 * {@link #elementList} fields for this instance
 	 */
 	private void initElements() {
 		this.generator = new GameGenerator(levelNum); //strict mvc pattern
-		this.frog = generator.getFrog();
+		this.player = generator.getPlayer();
 		this.elementList = generator.getList();
+		setScore(player.getScore());
+		notify("sprite", this);
 	}
 	
 	/**
@@ -254,64 +274,62 @@ public class GameModel {
 	 * 
 	 */
 	private void updateScoreList() {
-    	levelScoreList.add(new AbstractMap.SimpleEntry<Integer, Integer>(levelNum, frog.getScore()));
+		scoreString = "";
+    	levelScoreList.add(new AbstractMap.SimpleEntry<Integer, Integer>(levelNum, player.getScore()));
     	levelScoreList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
     	
     	 for (Map.Entry<Integer, Integer> entry : levelScoreList) {
+    		scoreString = scoreString + "Level " + entry.getKey() + "\t: " + entry.getValue() + "\n";
          	levels.add(entry.getKey().toString());
          	scores.add(entry.getValue().toString());
          }
     	 
 	}
 	
-	/**
-	 * This method creates an {@code AnimationTimer} 
-	 * object responsible for the bonus animation that
-	 * pops up when the {@link Frog} object catches a
-	 * fly
-	 */
-	private void createBonusTimer() {
-	       
-		bonusTimer = new AnimationTimer() {
-			@Override
-			public void handle(long now) {
-				showBonus(now);
-			}
-		};	
 
-    }
-	
-	
-	/**
-	 * This method contains the logic for bonus animation
-	 * (MVC pattern)
-	 * @param now  the timestamp of the current frame given 
-	 * in nanoseconds. 
-	 */
-	private void showBonus(long now) {
-		
-		if(now%11==0) {
-			animCtr+=1;
-		}
-		
-		if (animCtr==1) {
-			GameController.INSTANCE.setBonusVisible(true);
-		
-		}
-		
-		if(animCtr==5) {
-			animCtr=0;
-			GameController.INSTANCE.setBonusVisible(false);
-			hasBonus=false;
-			bonusTimer.stop();
-			
-		}
-	}
-	
-	
 
 	
 }
+
+///**
+// * This method creates an {@code AnimationTimer} 
+// * object responsible for the bonus animation that
+// * pops up when the {@link PlayerAvatar} object catches a
+// * fly
+// */
+//private void createBonusTimer() {
+//       
+//	bonusTimer = new AnimationTimer() {
+//		@Override
+//		public void handle(long now) {
+//			showBonus(now);
+//		}
+//	};	
+//
+//}
+
+
+///**
+// * This method contains the logic for bonus animation
+// * (MVC pattern)
+// * @param now  the timestamp of the current frame given 
+// * in nanoseconds. 
+// */
+//private void showBonus(long now) {
+//	
+//	if(now%11==0) {
+//		animCtr+=1;
+//	}
+//
+//	if(animCtr==5) {
+//		animCtr=0;
+//		hasBonus=false;
+//		notify("bonus",this);
+//		bonusTimer.stop();
+//		
+//	}
+//}
+
 
 
 //phase out
