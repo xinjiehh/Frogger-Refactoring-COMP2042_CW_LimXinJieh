1848d8d866c3d30c5d3b26ca2d803fce09cd79bd
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-14 18:28:29 +0800

    Refactor: extract code to ViewLoader and remove unnecessary fields

--- Frogger/src/main/java/frogger/controller/MenuController.java
+++ Frogger/src/main/java/frogger/controller/MenuController.java
@@ -1,43 +1,27 @@
 package frogger.controller;
 
-import java.io.IOException;
-
-import frogger.Main;
-import frogger.constant.FilePath;
+import frogger.util.ViewLoader;
+import frogger.view.MenuScreen;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 /**
  * This enum class implements singleton method. It acts as controller
- * for {@link MenuScreen} (View) which handles the different actions of the 
+ * for {@link MenuScreen} (View) which handles the different actions of the
  * menu buttons such as showing selection screen, showing info screen
  * and exiting the application
  * 
  */
 public enum MenuController {
+	
 	INSTANCE;
 	
-	private Pane pane = new Pane();
-	private FXMLLoader loader;
-	
+
 	/**
 	 * This method loads and shows the selection page from FXML for 
 	 * users to choose their controls
 	 */
 	public void viewSelection() {
-		try {
-			this.loader = new FXMLLoader(getClass().getResource(FilePath.VIEW_SELECT));
-		    this.pane = loader.load();
-		    Main.getPrimaryStage().getScene().setRoot(pane);
-		    Main.getPrimaryStage().show();
-		      
-		} catch (IOException e) {
-			System.out.println("Error loading selection page");
-			e.printStackTrace();
-		}
+		ViewLoader.INSTANCE.loadSelection();
 
 	}
 	
@@ -46,23 +30,7 @@ public enum MenuController {
 	 * a brief overview of the game
 	 */
 	public void showInfo() {
-		try {
-			FXMLLoader loader = new FXMLLoader((getClass().getResource(FilePath.INFO_FXML)));
-		    this.pane = loader.load();
-		   
-		    Stage infoStage = new Stage();
-		    infoStage.setTitle("Info");
-		    infoStage.setResizable(false);
-		    infoStage.initOwner(Main.getPrimaryStage().getScene().getWindow());
-		    infoStage.setScene(new Scene(pane));
-		   
-		    
-		    infoStage.show();
-		    
-		  } catch (IOException e) {
-			  System.out.println("Error loading info window in Screen Controller");
-			  e.printStackTrace();
-		  }
+		ViewLoader.INSTANCE.loadInfo();
 	}
 	
 	/**
