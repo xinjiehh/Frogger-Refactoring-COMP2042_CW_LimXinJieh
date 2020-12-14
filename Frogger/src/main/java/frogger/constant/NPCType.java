def172ab06fbd1ca1ae7240bf6b9d6cdc4b65f5f
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-14 18:46:41 +0800

    Docs: update Javadocs dependencies

--- Frogger/src/main/java/frogger/constant/NPCType.java
+++ Frogger/src/main/java/frogger/constant/NPCType.java
@@ -1,11 +1,13 @@
 package frogger.constant;
 
+import frogger.model.npc.NPC;
+
 /**
  * This interface
  * <u1>
  * <li> achieves loose coupling by reducing dependency of {@link NPC} 
  * on the enums. 
- * <li> improves extendability by tying {@link NPC} variation enums 
+ * <li> improves extendability by tying {@link NPC} variation enums
  * together (such as {@link LOG_TYPE} and {@link OBSTACLE_TYPE})
  * </li>
  * <p>
@@ -17,11 +19,11 @@ public interface NPCType {
 	 * This method returns the predefined size of the image
 	 * @return  integer value of the image size
 	 */
-	public int getSize();
+	int getSize();
 
 	/**
 	 * This method returns the predefined URL of the image
 	 * @return  {@code String} value of the image URL path
 	 */
-	public String getURL();
+	String getURL();
 }
