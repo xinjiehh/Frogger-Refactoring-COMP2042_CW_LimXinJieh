3035ad0dd91fac53b189a002eb0e415e106a031f
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-16 01:13:30 +0800

    Refactor: move Mode enum from SelectionController into own class

---
+++ Frogger/src/main/java/frogger/constant/settings/Mode.java
@@ -0,0 +1,11 @@
+package frogger.constant.settings;
+
+/**
+   * This enum represents the two modes for user to choose 
+   * from (to be implemented)
+   *
+   */
+  public enum Mode {
+	  SINGLE,
+	  DOUBLE
+  }
