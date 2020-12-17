a45057aaa66c41cdcffdd83cd04c5794e3c683e7
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-18 05:35:54 +0800

    Docs: add Javadocs

--- Frogger/src/main/java/frogger/constant/settings/Settings.java
+++ Frogger/src/main/java/frogger/constant/settings/Settings.java
@@ -1,9 +1,14 @@
 package frogger.constant.settings;
 
+/**
+ * This class enables maintainer to easily change the game settings
+ */
 public class Settings {
 	
+	/** the maximum number of frogs to reach the swamp to complete one level */
+	public static final int MAX_FROG = 5;
 
-	public static final int MAX_FROG = 2;
+	/** the maximum levels in a game */
 	public static final int MAX_LEVEL = 10;
 	
 
