4beed765ee61c39f6d0a9bdf20161eeb3e66faed
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-18 03:26:14 +0800

    Refactor: change visibility of fields

--- Frogger/src/main/java/frogger/controller/ProgressScreenController.java
+++ Frogger/src/main/java/frogger/controller/ProgressScreenController.java
@@ -34,12 +34,6 @@ public class ProgressScreenController {
 	private static final String PREFIX =
 			"-fx-background-image: url(\"" + FilePath.BUTTON_PATH;
 
-	/** inline css style of this {@code Button} when pressed */
-	private final String BUTTON_PRESSED = PREFIX + "button_pressed.png\");";
-
-	/** inline css style of this {@code Button} when pressed */
-	private final String BUTTON_UP = PREFIX + "button.png" + "\");";
-
 	/**
 	 * This method initializes the default font, game mode ({@link Mode#SINGLE})
 	 * and key controls ({@link Controls#WASD})
@@ -48,31 +42,48 @@ public class ProgressScreenController {
 	public void initialize() {
 		initFont();
 	}
-	
+
+	/**
+	 * This method sets the progress message to be displayed
+	 * @param str  {@code String} message to be displayed
+	 */
 	public void setText(String str) {
 		header.setText(str);
-		start.setText("START");
 	}
+
 	/**
-	 * This method initializes the font to be used in the related fxml file
-     */
-	private void initFont() {
-		Font font;
-		try {font = Font.loadFont(new FileInputStream(FilePath.DEFAULT_FONT), 20);} 
-		catch (FileNotFoundException e){
-			System.out.println("Error loading font for selection");
-			font = Font.font("Sans Serif", 20);
-		}
-		header.setFont(font);
-		start.setFont(font);
-	}
-
-
+	 * This method sets the property {@code onAction} of the button
+	 * @param x  {@code EventHandler} to be executed when button is pressed or
+	 * fired
+	 */
 	public void setButtonAction(EventHandler<ActionEvent> x) {
 		start.setOnAction(x);
 		
 	}
 
+	/**
+	 * This method sets the placeholder for this {@link Button}
+	 * object
+	 * @param buttonText  {@code String} placeholder for button
+	 */
+	public void setButtonText(String buttonText) {
+		start.setText(buttonText);
+	}
+
+	/**
+	 * This method handles key press on this screen
+	 * @param keyEvent  {@code KeyEvent} that fires
+	 */
+	@FXML
+	public void handleOnKeyPressed(KeyEvent keyEvent) {
+		start.fire();
+	}
+
+
+	/**
+	 * This method is used to handle the mouse event on the button
+	 * @param mouseEvent  {@code MouseEvent} fired on the button
+	 */
 	@FXML
 	public void handleMouseEvent(MouseEvent mouseEvent) {
 
@@ -99,10 +110,27 @@ public class ProgressScreenController {
 	}
 
 	/**
+	 * This method initializes the font to be used in the related fxml file
+	 */
+	private void initFont() {
+		Font font;
+		try {font = Font.loadFont(new FileInputStream(FilePath.DEFAULT_FONT), 20);}
+		catch (FileNotFoundException e){
+			System.out.println("Error loading font for selection");
+			font = Font.font("Sans Serif", 20);
+		}
+		header.setFont(font);
+		start.setFont(font);
+	}
+
+
+	/**
 	 * This method sets the style, preferred height and {@code layoutY}
 	 * property for a pressed button
 	 */
 	private void setButtonPressedStyle() {
+		//inline css style of this {@code Button} when pressed
+		String BUTTON_PRESSED = PREFIX + "button_pressed.png\");";
 		start.setStyle(BUTTON_PRESSED);
 		start.setPrefHeight(45);
 		start.setLayoutY(start.getLayoutY()+4);
@@ -115,19 +143,12 @@ public class ProgressScreenController {
 	 */
 
 	private void setButtonReleasedStyle() {
+		//inline css style of this {@code Button} when pressed
+		String BUTTON_UP = PREFIX + "button.png" + "\");";
 		start.setStyle(BUTTON_UP);
 		start.setPrefHeight(49);
 		start.setLayoutY(start.getLayoutY()-4);
 
 	}
 
-	@FXML
-	public void handleOnKeyPressed(KeyEvent keyEvent) {
-		System.out.println("here------");
-		start.fire();
-	}
-
-	public void setButtonText(String buttonText) {
-		start.setText(buttonText);
-	}
 }
