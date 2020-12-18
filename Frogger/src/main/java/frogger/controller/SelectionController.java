4b93ff11708f0c6ccffe28ca065e5e161026cb6a
Lim Xin Jieh <hcyxl2@nottingham.edu.my>
2020-12-19 02:11:54 +0800

    Refactor: customise start button and remove redundant styles

--- Frogger/src/main/java/frogger/controller/SelectionController.java
+++ Frogger/src/main/java/frogger/controller/SelectionController.java
@@ -1,13 +1,14 @@
 package frogger.controller;
 
 import frogger.constant.FilePath;
-import frogger.constant.settings.GameData;
 import frogger.constant.settings.Controls;
+import frogger.constant.settings.GameData;
 import frogger.constant.settings.Mode;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 
@@ -29,10 +30,15 @@ public class SelectionController {
 	@FXML private Button controls;
 	
 	/** the button to start game */ 
-	@FXML private Button start;
+	@FXML private Button startButton;
 
 	/** text field to enter player's name */
 	@FXML private TextField username;
+
+	/** inline css style of this {@code Button} */
+	private static final String PREFIX =
+			"-fx-background-image: url(\"" + FilePath.BUTTON_PATH;
+
 
 	/**
 	 * This method initializes the default font, game mode ({@link Mode#SINGLE})
@@ -49,14 +55,13 @@ public class SelectionController {
 	private void initNameListener() {
 		username.textProperty().addListener((obs, oldV, newV) -> {
 			String temp = newV;
-			//only allow letters
 			temp = temp.replaceAll("[^a-zA-Z]", "");
 			username.setText(temp);
 		});
 	}
 	/**
 	 * This method initializes the font to be used in the related fxml file
-     */
+	 */
 	private void initFont() {
 		try {
 			Font.loadFont(new FileInputStream(FilePath.DEFAULT_FONT), 20);}
@@ -66,16 +71,6 @@ public class SelectionController {
 
 	}
 
-
-	/**
-	 * This method allows users to switch between different playing 
-	 * modes (to be implemented)
-	 */
-	@FXML
-	public void switchMode() {
-	}
-
-  
 	/**
 	 * This method is responsible for updating the view and data 
 	 * when users select their key controls
@@ -95,9 +90,36 @@ public class SelectionController {
 	  
   }
 
+
 	/**
-	 * This method starts the game by calling {@link 
-	 * ScreenController} and passing the selected 
+	 * This method handles the {@code MouseEvent} on the button
+	 * @param mouseEvent  {@code MouseEvent} that fired on the button
+	 */
+	@FXML
+    public void handleMouseEvent(MouseEvent mouseEvent) {
+		switch(mouseEvent.getEventType().toString()) {
+			case "MOUSE_PRESSED":
+				setButtonPressedStyle();
+				break;
+			case "MOUSE_RELEASED":
+				setButtonReleasedStyle();
+				break;
+			case "MOUSE_ENTERED":
+				startButton.setEffect(new DropShadow());
+				break;
+			case "MOUSE_EXITED":
+				startButton.setEffect(null);
+				break;
+			default:
+				break;
+
+		}
+    }
+
+
+	/**
+	 * This method starts the game by calling {@link
+	 * ScreenController} and passing the selected
 	 * {@link Controls} to {@code ScreenController}
 	 */
 	@FXML
@@ -108,20 +130,30 @@ public class SelectionController {
 	}
 
 	/**
-	 * This method handles the {@code MouseEvent} on the button
-	 * @param mouseEvent  {@code MouseEvent} that fired on the button
+	 * This method sets the style, preferred height and {@code layoutY}
+	 * property for a pressed button
 	 */
-    public void handleMouseEvent(MouseEvent mouseEvent) {
-		switch(mouseEvent.getEventType().toString()) {
-			case "MOUSE_ENTERED":
-				start.setEffect(new DropShadow());
-				break;
-			case "MOUSE_EXITED":
-				start.setEffect(null);
-				break;
-			default:
-				break;
+	private void setButtonPressedStyle() {
+		//inline css style of this {@code Button} when pressed
+		String BUTTON_PRESSED = PREFIX + "button_pressed.png\");";
+		startButton.setStyle(BUTTON_PRESSED);
+		startButton.setPrefHeight(45);
+		startButton.setLayoutY(startButton.getLayoutY()+4);
 
-		}
-    }
+	}
+
+
+	/**
+	 * This method sets the style, preferred height and {@code layoutY}
+	 * property for a released button
+	 */
+
+	private void setButtonReleasedStyle() {
+		//inline css style of this {@code Button} when pressed
+		String BUTTON_UP = PREFIX + "button.png" + "\");";
+		startButton.setStyle(BUTTON_UP);
+		startButton.setPrefHeight(49);
+		startButton.setLayoutY(startButton.getLayoutY()-4);
+
+	}
 }
